a9fcb6419b16d56bec6cb5b30380bf5896252704
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2022-07-05 03:34:05 -0300

    create server and simple struture to optimize

---
+++ components/SeachResults.tsx
@@ -0,0 +1,23 @@
+import { ProductItem } from "./ProductItem"
+
+interface SearchResultsProps {
+    results : Array<{
+        id: number
+        title: string
+        price: number
+    }>
+}
+
+export function SearchResults({results}: SearchResultsProps) {
+    return (
+        <div>
+            {
+                results.map(product => {
+                    return (
+                        <ProductItem key={product.id} product={product} />
+                    )
+                })
+            }
+        </div>
+    )
+}
